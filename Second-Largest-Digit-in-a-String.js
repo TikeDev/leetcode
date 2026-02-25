7deb996a3745064c06e93d799fc652f8c3880453
KM <12630355+TikeDev@users.noreply.github.com>
2026-02-25 10:37:18 -0800

    [1796] [Time Beats: 55.96%] [Memory Beats: 18.51%] - LeetPush

--- Second-Largest-Digit-in-a-String.js
+++ Second-Largest-Digit-in-a-String.js
@@ -7,22 +7,10 @@
 7    for (let c of s){
 8      if (+c == c) sSet.add(+c);
 9    }
-10
-11    if (sSet.size < 2) return -1;
-12
-13    return Array.from(sSet).sort((a, b) => b-a)[1];
-14};
-15
-16/*
-17Input: alphanumeric string
-18Output: 2nd largest digit that appears in s, -1 if doesn't exist
-19
-20- filter for numbers
-21- if length === 1 return -1
-22- sort them desc, add to set
-23- if set.size < 1 return -1, return 2nd in set, 
-24
-25"dfa12321afd" => 2
-26"abc1111" => -1
-27
-28*/
+10    if (sSet.size < 2) return -1;
+11
+12    sSet.delete(Math.max(...sSet))
+13    return Math.max(...sSet)
+14
+15};
+16
